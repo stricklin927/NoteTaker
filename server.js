56a0d88b95f2c8ddad05ebecd5fdf400d22f5310
Alexandra Stricklin <stricklin927@gmail.com>
2021-03-03 01:28:34 -0500

    create and add routes to server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+// App Dependencies
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public")); 
+
+// Routes
+require("./public/assets/js/htmlRoutes")(app);
+
+// Listen
+app.listen(PORT, function() {
+  console.log("App is listening on PORT: " + PORT);
+});
